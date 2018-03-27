c17d93a362724dbd19a5f847caca8c749f1d1f75
Karimov R <rkarimov.r@gmail.com>
2018-03-28 01:09:23 +0400

    Add ApplicationListener

--- src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 import java.lang.management.ManagementFactory;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
@@ -37,22 +35,19 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
         Class beanClass = map.get(beanName);
         if (beanClass != null) {
             return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(),
-                    new InvocationHandler() {
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    if (controller.isEnabled()) {
-                        System.out.println("Профилирую...");
-                        long before = System.nanoTime();
-                        Object retVal = method.invoke(bean, args);
-                        long after = System.nanoTime();
-                        System.out.println(after - before);
-                        System.out.println("Всё");
-                        return retVal;
-                    } else {
-                        return method.invoke(bean, args);
-                    }
-                }
-            });
+                    (proxy, method, args) -> {
+                        if (controller.isEnabled()) {
+                            System.out.println("Профилирую...");
+                            long before = System.nanoTime();
+                            Object retVal = method.invoke(bean, args);
+                            long after = System.nanoTime();
+                            System.out.println(after - before);
+                            System.out.println("Всё");
+                            return retVal;
+                        } else {
+                            return method.invoke(bean, args);
+                        }
+                    });
         }
         return bean;
     }
