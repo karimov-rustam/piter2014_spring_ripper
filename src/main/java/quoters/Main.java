6ae9e53af9780e0a2ff51f43d8638d401a693679
Karimov R <rkarimov.r@gmail.com>
2018-03-27 23:59:28 +0400

    Implement BeanPostProcessor

---
+++ src/main/java/quoters/Main.java
@@ -0,0 +1,14 @@
+package quoters;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+
+        while (true) {
+            Thread.sleep(100);
+            context.getBean(Quoter.class).sayQuote();
+        }
+    }
+}
